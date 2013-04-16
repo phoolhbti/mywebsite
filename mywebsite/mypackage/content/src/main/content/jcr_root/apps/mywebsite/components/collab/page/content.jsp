a9536c92e193df992ad0b82e331a7930e414d52c
phool chandra <phoolhbti@gmail.com>
2013-04-16 14:45:48 +0530

    pc

---
+++ mywebsite/mypackage/content/src/main/content/jcr_root/apps/mywebsite/components/collab/page/content.jsp
@@ -0,0 +1,42 @@
+<%--
+  Copyright 1997-2009 Day Management AG
+  Barfuesserplatz 6, 4001 Basel, Switzerland
+  All Rights Reserved.
+
+  This software is the confidential and proprietary information of
+  Day Management AG, ("Confidential Information"). You shall not
+  disclose such Confidential Information and shall use it only in
+  accordance with the terms of the license agreement you entered into
+  with Day.
+
+  ==============================================================================
+
+  Blog: Content script (included by body.jsp)
+
+  ==============================================================================
+
+--%><%@ page import="com.day.cq.collab.blog.Blog,
+                     com.day.cq.collab.blog.BlogManager" %><%
+%><%@include file="/libs/foundation/global.jsp" %><%
+
+    BlogManager blogMgr = resource.getResourceResolver().adaptTo(BlogManager.class);
+    Blog blog = blogMgr.getBlog(slingRequest);
+
+%><div id="content" class="<%= blog.isEntry() ? "widecolumn" : "narrowcolumn" %>">
+
+<cq:include path="blog" resourceType="collab/blog/components/main" />
+
+</div>
+<%
+
+    if (!blog.isEntry()) {
+    
+    %>
+    <div id="sidebar">
+        <ul>
+            <cq:include path="sidebar" resourceType="foundation/components/parsys"/>
+        </ul>
+    </div>
+    <%
+    }
+%>
---
+++ mywebsite/mypackage/content/src/main/content/jcr_root/apps/mywebsite/src/impl/src/main/java/com/pc/test/cqutil/servlets/ProductServletPC.java
@@ -0,0 +1,59 @@
+package com.pc.test.cqutil.servlets;
+
+import java.io.IOException;
+import org.apache.felix.scr.annotations.Properties;
+import org.apache.felix.scr.annotations.Property;
+import org.apache.felix.scr.annotations.Reference;
+import org.apache.felix.scr.annotations.sling.SlingServlet;
+import org.apache.sling.api.SlingHttpServletRequest;
+import org.apache.sling.api.SlingHttpServletResponse;
+import org.apache.sling.api.servlets.SlingAllMethodsServlet;
+
+import javax.jcr.Session;
+import javax.servlet.ServletException;
+import java.util.Map;
+import java.util.HashMap;
+
+//import com.pearson.manager.ProductManagerService;
+//import com.pearson.model.ProductBean;
+
+import org.apache.sling.commons.json.JSONException;
+import org.apache.sling.commons.json.JSONObject;
+import org.apache.sling.commons.json.JSONArray;
+
+@SlingServlet(paths = { "/apps/services/productPC.html" })
+@Properties( {
+        @Property(name = "service.pid", value = "com.pearson.servlets.ProductServletPC", propertyPrivate = false),
+        @Property(name = "service.description", value = "ProductServletPC", propertyPrivate = false),
+        @Property(name = "service.vendor", value = "VirtusaPC", propertyPrivate = false) })
+public class ProductServletPC extends SlingAllMethodsServlet {
+   
+
+    @Override
+    protected void doGet(SlingHttpServletRequest request,
+            SlingHttpServletResponse response) throws ServletException,
+            IOException {
+             System.out.println("Entering into get PC servelt");
+           try{  
+                    response.getWriter().write("Hello worldllllllllllllllllll");
+                    
+                     //response.getWriter().write("Page Created");
+                     // response.getWriter().write(productManagerService.search(mockValues).get(0).getTitle());
+           
+            }catch(Exception e){
+            
+            System.out.println(e);
+            e.printStackTrace();
+        }
+            
+            
+      
+    }
+
+    @Override
+    protected void doPost(SlingHttpServletRequest request,
+            SlingHttpServletResponse response) throws ServletException,
+            IOException {
+        doGet(request, response);
+    }
+}
