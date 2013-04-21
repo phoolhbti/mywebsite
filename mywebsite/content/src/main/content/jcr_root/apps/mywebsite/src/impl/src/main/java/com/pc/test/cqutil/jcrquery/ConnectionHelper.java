a123213a3cd5e22d8c233221bb269eebb467c4c8
phool <phoolhbti@gmail.com>
2013-04-21 22:18:40 +0530

    hee

---
+++ mywebsite/content/src/main/content/jcr_root/apps/mywebsite/src/impl/src/main/java/com/pc/test/cqutil/jcrquery/ConnectionHelper.java
@@ -0,0 +1,40 @@
+package com.pc.test.cqutil.jcrquery;
+import java.sql.Connection; 
+import java.sql.DriverManager; 
+import java.sql.SQLException; 
+ 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+  
+public class ConnectionHelper 
+{ 
+/** Default log. */
+protected final Logger log = LoggerFactory.getLogger(this.getClass());
+ private String url; 
+ private static ConnectionHelper instance; 
+ private ConnectionHelper() 
+{ 
+    url = "jdbc:mysql://localhost:3306/CQ"; 
+} 
+public static Connection getConnection() throws SQLException { 
+
+ if (instance == null) { 
+    instance = new ConnectionHelper(); 
+} 
+try { 
+  return DriverManager.getConnection(instance.url, "root","root"); 
+} catch (SQLException e) { 
+ throw e; 
+ } 
+} 
+public static void close(Connection connection) 
+{ 
+try { 
+  if (connection != null) { 
+   connection.close(); 
+ } 
+} catch (SQLException e) { 
+e.printStackTrace(); 
+} 
+} 
+}
